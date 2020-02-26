f4475cffd5bee78d4f49af7e7b6a4586de27c36d
vjpal3 <vrishalipal@yahoo.com>
2020-02-25 21:55:08 -0500

    Initial Commit

--- SingletonPattern/SingletonDemo.cs
+++ SingletonPattern/SingletonDemo.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace SingletonPattern
 {
-    class Program
+    class SingletonDemo
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
