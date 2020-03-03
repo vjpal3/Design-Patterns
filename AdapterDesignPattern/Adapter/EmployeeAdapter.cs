f04429935326289a686f61233dbdd9b77e04af5a
vjpal3 <vrishalipal@yahoo.com>
2020-03-02 20:03:00 -0500

    Implement Adapter Design Pattern

---
+++ AdapterDesignPattern/Adapter/EmployeeAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using AdapterDesignPattern.Target;
+using Newtonsoft.Json;
+
+namespace AdapterDesignPattern.Adapter
+{
+    public class EmployeeAdapter : EmployeeManager, IEmployee
+    {
+        public override string GetAllEmployees()
+        {
+            string returnXml = base.GetAllEmployees();
+            var doc = new XmlDocument();
+            doc.LoadXml(returnXml);
+            return JsonConvert.SerializeObject(doc, Newtonsoft.Json.Formatting.Indented);
+        }
+    }
+}
---
+++ AdapterDesignPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdapterDesignPattern.Adapter;
+using AdapterDesignPattern.Target;
+
+namespace AdapterDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEmployee emp = new EmployeeAdapter();
+            string value = emp.GetAllEmployees();
+            Console.ReadLine();
+        }
+    }
+}
